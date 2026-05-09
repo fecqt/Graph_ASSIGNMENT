7c3c42be6467bb6057604a5d2a5dbe051d38105b
ChislucaAna <139362209+ChislucaAna@users.noreply.github.com>
2026-05-09 15:17:09 +0300

    fixed missing NumberOfDistinctPaths file

---
+++ DAG/NumberOfDistinctPaths.py
@@ -0,0 +1,16 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from Graph import Graph
+from DAG.TopologicalSort import topological_sort
+
+def count_paths_dag(graph, start, end):
+    topo = topological_sort(graph)
+
+    dp = {v: 0 for v in graph.get_vertices()}
+    dp[start] = 1
+
+    for u in topo:
+        for v in graph.neighbors(u):
+            dp[v] += dp[u]
+
+    return dp[end]
